0a37ca3ccf8c11e79c596d8930183c9d2e32a867
sharmas75 <71354509+sharmas75@users.noreply.github.com>
2023-08-31 14:01:04 -0600

    change summary

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,25 +1,22 @@
 //summary page descriptions
 export const summary = [
-  "Here's some info about me: ",
-  "I Graduated from Macewan University with a degree in Computer Science",
-  "Coursework mainly covered C and Python programming, embedded systems, networking, using security, intrusion prevention and detection tools",
-  "Some school projects I worked on:",
-  "Developed an IoT monitoring system that uses several physical sensors to track blood pressure, heart rate, and temperature and displays it on a mobile application as a final project for my program",
-  "Developed a python keyword syllabi search program in my spare time to help count the frequencies of specified keywords in a given list, then displayed that information graphically",
-  "Developed a property value search program in Java with a partner for a school project. Included built-in google maps functionality as well the capability to search through a list containing hundreds of thousands of values in a timely manner,",
-  
+  "Welcome to my website! Here's some info about me: ",
+  "I am a recent Computer Science graduate from Macewan University looking to get into the industry",
+  "My Coursework mainly covered C and Python programming, embedded systems, networking, and systems information and security. ",
+    " My main areas of expertise are Python, C and I am learning React as well as Javascript, CSS, and HTML. ",
+  "I enjoy playing basketball and videogames and am trying to get into hiking when I have time."
 ];
 
 // project page descriptions
 export const projects = {
-    project0: [
-        "Personal Website",
-        [
-          "I hosted this website on AWS lightsail using a virtual Ubuntu instance that has NGINX installed.",
-          "Domain and certificates configured using AWS route 53 and Lightsail console. Website itself ",
-          "is made in React, which is composed of Javascript, HTML, and CSS. ",
-        ],
-      ],
+  project0: [
+    "Personal Website",
+    [
+      "I hosted this website on AWS lightsail using a virtual Ubuntu instance that has NGINX installed.",
+      "Domain and certificates configured using AWS route 53 and Lightsail console. Website itself ",
+      "is made in React, which is composed of Javascript, HTML, and CSS. ",
+    ],
+  ],
   project1: [
     "Rudimentary IoT Health Monitoring System",
     [
@@ -31,8 +28,8 @@ export const projects = {
   project2: [
     "Syllabi Search Program",
     [
-      "Matched keywords in a chosen PDF document to a pre-determined key words list that then displayed results", 
-      "(i.e. graphs, tables, matched keywords) in a readable UI. Some functionality included: enabled users to save", 
+      "Matched keywords in a chosen PDF document to a pre-determined key words list that then displayed results",
+      "(i.e. graphs, tables, matched keywords) in a readable UI. Some functionality included: enabled users to save",
       "and load: previous results, settings that customized UI, able to view results in different grapg formats (bar, pie, histogram)",
     ],
   ],
@@ -42,19 +39,17 @@ export const projects = {
       "Allowed a user to search for specific property values in Edmonton with a given dataset. Some features included: ",
       "A built-in Google maps feature to allow visualization of property locations when searching for properties. ",
       "was able to search by address, and values. Designed the program specifically with scalability and extendibility for the future",
-      
     ],
   ],
   project4: [
     "Hosting Gitlab on a Linux Server",
     [
-      "Set up a firewall, and partitioned disk storage for increased security, Installed intrusion detection systems such as tripwire,", 
+      "Set up a firewall, and partitioned disk storage for increased security, Installed intrusion detection systems such as tripwire,",
       "and system monitoring while performing regular security audits. Set up user accounts and assigned permissions according to a need-to-know basis",
       "Performed automated tests using pentesting tools such as John the ripper, chkrootkit and Lynis, to check for system health and security",
-      "Wrote user guides and security reports on system."
+      "Wrote user guides and security reports on system.",
     ],
   ],
-  
 };
 
 export const languages = [
@@ -68,18 +63,19 @@ export const languages = [
   "Javascript",
   "HTML",
   "CSS",
-  "Assembly Language"
+  "Assembly Language",
 ];
 
-export const technologies = ["Git",
-"Linux",
-"Windows",
-"Amazon Lightsail",
-"Amazon firebase",
-"VsCode",
-"Ardunio IDE",
-"Linux/Unix Cli",
-]
+export const technologies = [
+  "Git",
+  "Linux",
+  "Windows",
+  "Amazon Lightsail",
+  "Amazon firebase",
+  "VsCode",
+  "Ardunio IDE",
+  "Linux/Unix Cli",
+];
 
 export const links = {
   Email: "www.shantsharma.com",
@@ -88,29 +84,29 @@ export const links = {
 };
 
 export const pageVariants = {
-    initial: {
-      opacity: 0,
-      x: "-100vw",
-      scale: 0.8
-    },
-    in: {
-      opacity: 1,
-      x: 0,
-      scale: 1
-    },
-    out: {
-      opacity: 0,
-      x: "100vw",
-      scale: 0.5
-    }
-  };
-  
-  export const pageTransition = {
-    type: "tween",
-    ease: "anticipate",
-    duration: 0.7
-  };
-  
-  export const pageStyle = {
-    position: "absolute"
-  };
+  initial: {
+    opacity: 0,
+    x: "-100vw",
+    scale: 0.8,
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+  },
+  out: {
+    opacity: 0,
+    x: "100vw",
+    scale: 0.5,
+  },
+};
+
+export const pageTransition = {
+  type: "tween",
+  ease: "anticipate",
+  duration: 0.7,
+};
+
+export const pageStyle = {
+  position: "absolute",
+};
